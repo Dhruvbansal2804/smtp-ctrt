fa6b1071443a37697fc978ed12a9676f532f4868
Dhruv Bansal <dhruvbansal2804@gmail.com>
2024-10-10 22:28:32 +0530

    added url config for password reset

--- ctr/urls.py
+++ ctr/urls.py
@@ -32,3 +32,19 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views  # Import authentication views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Other URL patterns for your application...
+
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
